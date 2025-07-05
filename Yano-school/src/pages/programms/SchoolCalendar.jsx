a0b9ad71b44c5c0dd75e3cafaaf54ec6535a0177
davidyano600 <godsentryan@gmail.com>
2025-07-05 19:30:13 +0100

    ok

--- Yano-school/src/pages/programms/SchoolCalendar.jsx
+++ Yano-school/src/pages/programms/SchoolCalendar.jsx
@@ -1,7 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faCalendarAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 export default function SchoolCalendar() {
+  const base = import.meta.env.BASE_URL;
+
   return (
     <section className="mb-16 bg-gray-100 px-4 py-8 rounded-2xl shadow-inner">
       <h2 className="text-2xl font-semibold text-gray-700 mb-4 flex items-center">
@@ -18,7 +20,7 @@ export default function SchoolCalendar() {
 
       <div className="flex flex-col sm:flex-row gap-4">
         <a
-          href="/files/lagos.pdf"
+          href={`${base}files/lagos.pdf`}
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center px-6 py-2 bg-red-400 text-white rounded-full hover:bg-red-500 transition text-sm shadow"
@@ -26,8 +28,6 @@ export default function SchoolCalendar() {
           <FontAwesomeIcon icon={faDownload} className="mr-2" />
           View PDF
         </a>
-
-        
       </div>
     </section>
   );
